c3de1c40ac5e32abccb31a895fbc9a7fd41764d9
Lahtinen Kalle <ao186696@gradia.fi>
2019-07-06 17:33:23 +0300

    Lisätty lahjakortin lisäys formi

---
+++ Lahjakorttiappi/Lisaaminen.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace Lahjakorttiappi
+{
+    partial class Lisaaminen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlControls = new System.Windows.Forms.Panel();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEmpty = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtBoxFristName = new System.Windows.Forms.TextBox();
+            this.txtBoxLastName = new System.Windows.Forms.TextBox();
+            this.TxtBoxAdress = new System.Windows.Forms.TextBox();
+            this.txtBoxPoNbr = new System.Windows.Forms.TextBox();
+            this.txtBoxPoPlace = new System.Windows.Forms.TextBox();
+            this.txtBoxPhone = new System.Windows.Forms.TextBox();
+            this.txtBoxEmail = new System.Windows.Forms.TextBox();
+            this.lblFristName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblAdress = new System.Windows.Forms.Label();
+            this.lblPo = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.pnlControls.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AccessibleDescription = "Tietojen lisäys paneeli";
+            this.panel1.Controls.Add(this.lblEmail);
+            this.panel1.Controls.Add(this.lblPhone);
+            this.panel1.Controls.Add(this.lblPo);
+            this.panel1.Controls.Add(this.lblAdress);
+            this.panel1.Controls.Add(this.lblLastName);
+            this.panel1.Controls.Add(this.lblFristName);
+            this.panel1.Controls.Add(this.txtBoxEmail);
+            this.panel1.Controls.Add(this.txtBoxPhone);
+            this.panel1.Controls.Add(this.txtBoxPoPlace);
+            this.panel1.Controls.Add(this.txtBoxPoNbr);
+            this.panel1.Controls.Add(this.TxtBoxAdress);
+            this.panel1.Controls.Add(this.txtBoxLastName);
+            this.panel1.Controls.Add(this.txtBoxFristName);
+            this.panel1.Location = new System.Drawing.Point(12, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(589, 425);
+            this.panel1.TabIndex = 0;
+            // 
+            // pnlControls
+            // 
+            this.pnlControls.Controls.Add(this.btnExit);
+            this.pnlControls.Controls.Add(this.btnEmpty);
+            this.pnlControls.Controls.Add(this.btnAdd);
+            this.pnlControls.Location = new System.Drawing.Point(607, 13);
+            this.pnlControls.Name = "pnlControls";
+            this.pnlControls.Size = new System.Drawing.Size(181, 425);
+            this.pnlControls.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.AccessibleName = "Lisäys painike";
+            this.btnAdd.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(15, 15);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(152, 71);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Lisää";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnEmpty
+            // 
+            this.btnEmpty.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmpty.Location = new System.Drawing.Point(15, 92);
+            this.btnEmpty.Name = "btnEmpty";
+            this.btnEmpty.Size = new System.Drawing.Size(152, 71);
+            this.btnEmpty.TabIndex = 1;
+            this.btnEmpty.Text = "Tyhjennä";
+            this.btnEmpty.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(15, 169);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(152, 71);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Poistu";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // txtBoxFristName
+            // 
+            this.txtBoxFristName.AccessibleName = "Etunimi";
+            this.txtBoxFristName.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxFristName.Location = new System.Drawing.Point(178, 43);
+            this.txtBoxFristName.Name = "txtBoxFristName";
+            this.txtBoxFristName.Size = new System.Drawing.Size(260, 29);
+            this.txtBoxFristName.TabIndex = 0;
+            // 
+            // txtBoxLastName
+            // 
+            this.txtBoxLastName.AccessibleName = "Sukunimi";
+            this.txtBoxLastName.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxLastName.Location = new System.Drawing.Point(178, 78);
+            this.txtBoxLastName.Name = "txtBoxLastName";
+            this.txtBoxLastName.Size = new System.Drawing.Size(260, 29);
+            this.txtBoxLastName.TabIndex = 1;
+            // 
+            // TxtBoxAdress
+            // 
+            this.TxtBoxAdress.AccessibleName = "Osoite";
+            this.TxtBoxAdress.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBoxAdress.Location = new System.Drawing.Point(178, 113);
+            this.TxtBoxAdress.Name = "TxtBoxAdress";
+            this.TxtBoxAdress.Size = new System.Drawing.Size(260, 29);
+            this.TxtBoxAdress.TabIndex = 2;
+            // 
+            // txtBoxPoNbr
+            // 
+            this.txtBoxPoNbr.AccessibleName = "Postinumero";
+            this.txtBoxPoNbr.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxPoNbr.Location = new System.Drawing.Point(178, 148);
+            this.txtBoxPoNbr.Name = "txtBoxPoNbr";
+            this.txtBoxPoNbr.Size = new System.Drawing.Size(110, 29);
+            this.txtBoxPoNbr.TabIndex = 3;
+            // 
+            // txtBoxPoPlace
+            // 
+            this.txtBoxPoPlace.AccessibleName = "Postitoimipaikka";
+            this.txtBoxPoPlace.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxPoPlace.Location = new System.Drawing.Point(294, 148);
+            this.txtBoxPoPlace.Name = "txtBoxPoPlace";
+            this.txtBoxPoPlace.Size = new System.Drawing.Size(144, 29);
+            this.txtBoxPoPlace.TabIndex = 4;
+            // 
+            // txtBoxPhone
+            // 
+            this.txtBoxPhone.AccessibleName = "Puhelinnumero";
+            this.txtBoxPhone.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxPhone.Location = new System.Drawing.Point(178, 183);
+            this.txtBoxPhone.Name = "txtBoxPhone";
+            this.txtBoxPhone.Size = new System.Drawing.Size(260, 29);
+            this.txtBoxPhone.TabIndex = 5;
+            // 
+            // txtBoxEmail
+            // 
+            this.txtBoxEmail.AccessibleName = "Sähköposti";
+            this.txtBoxEmail.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxEmail.Location = new System.Drawing.Point(178, 218);
+            this.txtBoxEmail.Name = "txtBoxEmail";
+            this.txtBoxEmail.Size = new System.Drawing.Size(260, 29);
+            this.txtBoxEmail.TabIndex = 6;
+            // 
+            // lblFristName
+            // 
+            this.lblFristName.AutoSize = true;
+            this.lblFristName.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFristName.Location = new System.Drawing.Point(15, 45);
+            this.lblFristName.Name = "lblFristName";
+            this.lblFristName.Size = new System.Drawing.Size(84, 24);
+            this.lblFristName.TabIndex = 7;
+            this.lblFristName.Text = "Etunimi:";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastName.Location = new System.Drawing.Point(15, 80);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(99, 24);
+            this.lblLastName.TabIndex = 8;
+            this.lblLastName.Text = "Sukunimi:";
+            // 
+            // lblAdress
+            // 
+            this.lblAdress.AutoSize = true;
+            this.lblAdress.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAdress.Location = new System.Drawing.Point(15, 115);
+            this.lblAdress.Name = "lblAdress";
+            this.lblAdress.Size = new System.Drawing.Size(72, 24);
+            this.lblAdress.TabIndex = 9;
+            this.lblAdress.Text = "Osoite";
+            // 
+            // lblPo
+            // 
+            this.lblPo.AutoSize = true;
+            this.lblPo.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPo.Location = new System.Drawing.Point(15, 150);
+            this.lblPo.Name = "lblPo";
+            this.lblPo.Size = new System.Drawing.Size(50, 24);
+            this.lblPo.TabIndex = 10;
+            this.lblPo.Text = "PTP";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPhone.Location = new System.Drawing.Point(15, 183);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblPhone.Size = new System.Drawing.Size(147, 24);
+            this.lblPhone.TabIndex = 11;
+            this.lblPhone.Text = "Puhelinnumero";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(15, 220);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(114, 24);
+            this.lblEmail.TabIndex = 12;
+            this.lblEmail.Text = "Sähköposti";
+            // 
+            // Lisaaminen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlControls);
+            this.Controls.Add(this.panel1);
+            this.Name = "Lisaaminen";
+            this.Text = "Lisaaminen";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.pnlControls.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblPo;
+        private System.Windows.Forms.Label lblAdress;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblFristName;
+        private System.Windows.Forms.TextBox txtBoxEmail;
+        private System.Windows.Forms.TextBox txtBoxPhone;
+        private System.Windows.Forms.TextBox txtBoxPoPlace;
+        private System.Windows.Forms.TextBox txtBoxPoNbr;
+        private System.Windows.Forms.TextBox TxtBoxAdress;
+        private System.Windows.Forms.TextBox txtBoxLastName;
+        private System.Windows.Forms.TextBox txtBoxFristName;
+        private System.Windows.Forms.Panel pnlControls;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnEmpty;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
