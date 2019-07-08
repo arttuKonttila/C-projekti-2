588efb73ad93ca7a38ab54b3dbfe0d897e3d11b9
Lahtinen Kalle <ao186696@gradia.fi>
2019-07-08 20:52:24 +0300

    Lisätty Lisääminen formiin myös lahjakortin tiedot

--- Lahjakorttiappi/Lisaaminen.Designer.cs
+++ Lahjakorttiappi/Lisaaminen.Designer.cs
@@ -47,8 +47,21 @@
             this.btnEmpty = new System.Windows.Forms.Button();
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnSend = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dtmSellTime = new System.Windows.Forms.DateTimePicker();
+            this.numAmount = new System.Windows.Forms.NumericUpDown();
+            this.cmBoxService = new System.Windows.Forms.ComboBox();
+            this.cmBoxTime = new System.Windows.Forms.ComboBox();
+            this.lblService = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblAmmount = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.cmBoxSeller = new System.Windows.Forms.ComboBox();
+            this.lblSeller = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.pnlControls.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numAmount)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -69,14 +82,14 @@
             this.panel1.Controls.Add(this.txtBoxFristName);
             this.panel1.Location = new System.Drawing.Point(12, 13);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(589, 425);
+            this.panel1.Size = new System.Drawing.Size(589, 254);
             this.panel1.TabIndex = 0;
             // 
             // lblEmail
             // 
             this.lblEmail.AutoSize = true;
             this.lblEmail.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEmail.Location = new System.Drawing.Point(15, 220);
+            this.lblEmail.Location = new System.Drawing.Point(50, 199);
             this.lblEmail.Name = "lblEmail";
             this.lblEmail.Size = new System.Drawing.Size(114, 24);
             this.lblEmail.TabIndex = 12;
@@ -86,7 +99,7 @@
             // 
             this.lblPhone.AutoSize = true;
             this.lblPhone.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPhone.Location = new System.Drawing.Point(15, 183);
+            this.lblPhone.Location = new System.Drawing.Point(50, 162);
             this.lblPhone.Name = "lblPhone";
             this.lblPhone.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.lblPhone.Size = new System.Drawing.Size(147, 24);
@@ -97,7 +110,7 @@
             // 
             this.lblPo.AutoSize = true;
             this.lblPo.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPo.Location = new System.Drawing.Point(15, 150);
+            this.lblPo.Location = new System.Drawing.Point(50, 129);
             this.lblPo.Name = "lblPo";
             this.lblPo.Size = new System.Drawing.Size(50, 24);
             this.lblPo.TabIndex = 10;
@@ -107,7 +120,7 @@
             // 
             this.lblAdress.AutoSize = true;
             this.lblAdress.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAdress.Location = new System.Drawing.Point(15, 115);
+            this.lblAdress.Location = new System.Drawing.Point(50, 94);
             this.lblAdress.Name = "lblAdress";
             this.lblAdress.Size = new System.Drawing.Size(72, 24);
             this.lblAdress.TabIndex = 9;
@@ -117,7 +130,7 @@
             // 
             this.lblLastName.AutoSize = true;
             this.lblLastName.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLastName.Location = new System.Drawing.Point(15, 80);
+            this.lblLastName.Location = new System.Drawing.Point(50, 59);
             this.lblLastName.Name = "lblLastName";
             this.lblLastName.Size = new System.Drawing.Size(99, 24);
             this.lblLastName.TabIndex = 8;
@@ -127,7 +140,7 @@
             // 
             this.lblFristName.AutoSize = true;
             this.lblFristName.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFristName.Location = new System.Drawing.Point(15, 45);
+            this.lblFristName.Location = new System.Drawing.Point(50, 24);
             this.lblFristName.Name = "lblFristName";
             this.lblFristName.Size = new System.Drawing.Size(84, 24);
             this.lblFristName.TabIndex = 7;
@@ -137,7 +150,7 @@
             // 
             this.txtBoxEmail.AccessibleName = "Sähköposti";
             this.txtBoxEmail.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBoxEmail.Location = new System.Drawing.Point(178, 218);
+            this.txtBoxEmail.Location = new System.Drawing.Point(213, 197);
             this.txtBoxEmail.Name = "txtBoxEmail";
             this.txtBoxEmail.Size = new System.Drawing.Size(260, 29);
             this.txtBoxEmail.TabIndex = 6;
@@ -146,7 +159,7 @@
             // 
             this.txtBoxPhone.AccessibleName = "Puhelinnumero";
             this.txtBoxPhone.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBoxPhone.Location = new System.Drawing.Point(178, 183);
+            this.txtBoxPhone.Location = new System.Drawing.Point(213, 162);
             this.txtBoxPhone.Name = "txtBoxPhone";
             this.txtBoxPhone.Size = new System.Drawing.Size(260, 29);
             this.txtBoxPhone.TabIndex = 5;
@@ -155,7 +168,7 @@
             // 
             this.txtBoxPoPlace.AccessibleName = "Postitoimipaikka";
             this.txtBoxPoPlace.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBoxPoPlace.Location = new System.Drawing.Point(294, 148);
+            this.txtBoxPoPlace.Location = new System.Drawing.Point(329, 127);
             this.txtBoxPoPlace.Name = "txtBoxPoPlace";
             this.txtBoxPoPlace.Size = new System.Drawing.Size(144, 29);
             this.txtBoxPoPlace.TabIndex = 4;
@@ -164,7 +177,7 @@
             // 
             this.txtBoxPoNbr.AccessibleName = "Postinumero";
             this.txtBoxPoNbr.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBoxPoNbr.Location = new System.Drawing.Point(178, 148);
+            this.txtBoxPoNbr.Location = new System.Drawing.Point(213, 127);
             this.txtBoxPoNbr.Name = "txtBoxPoNbr";
             this.txtBoxPoNbr.Size = new System.Drawing.Size(110, 29);
             this.txtBoxPoNbr.TabIndex = 3;
@@ -173,7 +186,7 @@
             // 
             this.TxtBoxAdress.AccessibleName = "Osoite";
             this.TxtBoxAdress.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TxtBoxAdress.Location = new System.Drawing.Point(178, 113);
+            this.TxtBoxAdress.Location = new System.Drawing.Point(213, 92);
             this.TxtBoxAdress.Name = "TxtBoxAdress";
             this.TxtBoxAdress.Size = new System.Drawing.Size(260, 29);
             this.TxtBoxAdress.TabIndex = 2;
@@ -182,7 +195,7 @@
             // 
             this.txtBoxLastName.AccessibleName = "Sukunimi";
             this.txtBoxLastName.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBoxLastName.Location = new System.Drawing.Point(178, 78);
+            this.txtBoxLastName.Location = new System.Drawing.Point(213, 57);
             this.txtBoxLastName.Name = "txtBoxLastName";
             this.txtBoxLastName.Size = new System.Drawing.Size(260, 29);
             this.txtBoxLastName.TabIndex = 1;
@@ -191,7 +204,7 @@
             // 
             this.txtBoxFristName.AccessibleName = "Etunimi";
             this.txtBoxFristName.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBoxFristName.Location = new System.Drawing.Point(178, 43);
+            this.txtBoxFristName.Location = new System.Drawing.Point(213, 22);
             this.txtBoxFristName.Name = "txtBoxFristName";
             this.txtBoxFristName.Size = new System.Drawing.Size(260, 29);
             this.txtBoxFristName.TabIndex = 0;
@@ -250,11 +263,126 @@
             this.btnSend.Text = "Lähetä";
             this.btnSend.UseVisualStyleBackColor = true;
             // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lblSeller);
+            this.panel2.Controls.Add(this.cmBoxSeller);
+            this.panel2.Controls.Add(this.lblDate);
+            this.panel2.Controls.Add(this.lblAmmount);
+            this.panel2.Controls.Add(this.lblTime);
+            this.panel2.Controls.Add(this.lblService);
+            this.panel2.Controls.Add(this.cmBoxTime);
+            this.panel2.Controls.Add(this.cmBoxService);
+            this.panel2.Controls.Add(this.numAmount);
+            this.panel2.Controls.Add(this.dtmSellTime);
+            this.panel2.Location = new System.Drawing.Point(12, 273);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(588, 164);
+            this.panel2.TabIndex = 2;
+            // 
+            // dtmSellTime
+            // 
+            this.dtmSellTime.CalendarFont = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtmSellTime.Location = new System.Drawing.Point(374, 37);
+            this.dtmSellTime.Name = "dtmSellTime";
+            this.dtmSellTime.Size = new System.Drawing.Size(211, 20);
+            this.dtmSellTime.TabIndex = 0;
+            // 
+            // numAmount
+            // 
+            this.numAmount.AccessibleName = "Kerrtojen märä";
+            this.numAmount.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numAmount.Location = new System.Drawing.Point(213, 37);
+            this.numAmount.Name = "numAmount";
+            this.numAmount.Size = new System.Drawing.Size(120, 32);
+            this.numAmount.TabIndex = 1;
+            // 
+            // cmBoxService
+            // 
+            this.cmBoxService.AccessibleName = "Palvelu ";
+            this.cmBoxService.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmBoxService.FormattingEnabled = true;
+            this.cmBoxService.Location = new System.Drawing.Point(13, 37);
+            this.cmBoxService.Name = "cmBoxService";
+            this.cmBoxService.Size = new System.Drawing.Size(151, 32);
+            this.cmBoxService.TabIndex = 0;
+            // 
+            // cmBoxTime
+            // 
+            this.cmBoxTime.AccessibleName = "Kesto";
+            this.cmBoxTime.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmBoxTime.FormattingEnabled = true;
+            this.cmBoxTime.Location = new System.Drawing.Point(13, 110);
+            this.cmBoxTime.Name = "cmBoxTime";
+            this.cmBoxTime.Size = new System.Drawing.Size(151, 32);
+            this.cmBoxTime.TabIndex = 2;
+            // 
+            // lblService
+            // 
+            this.lblService.AutoSize = true;
+            this.lblService.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblService.Location = new System.Drawing.Point(9, 9);
+            this.lblService.Name = "lblService";
+            this.lblService.Size = new System.Drawing.Size(78, 24);
+            this.lblService.TabIndex = 3;
+            this.lblService.Text = "Palvelu";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(9, 83);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(68, 24);
+            this.lblTime.TabIndex = 4;
+            this.lblTime.Text = "Pituus";
+            // 
+            // lblAmmount
+            // 
+            this.lblAmmount.AutoSize = true;
+            this.lblAmmount.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAmmount.Location = new System.Drawing.Point(209, 9);
+            this.lblAmmount.Name = "lblAmmount";
+            this.lblAmmount.Size = new System.Drawing.Size(70, 24);
+            this.lblAmmount.TabIndex = 5;
+            this.lblAmmount.Text = "Määrä";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.Location = new System.Drawing.Point(370, 9);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(186, 24);
+            this.lblDate.TabIndex = 6;
+            this.lblDate.Text = "Voimaan tulo päivä";
+            // 
+            // cmBoxSeller
+            // 
+            this.cmBoxSeller.AccessibleName = "Myyjä";
+            this.cmBoxSeller.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmBoxSeller.FormattingEnabled = true;
+            this.cmBoxSeller.Location = new System.Drawing.Point(213, 110);
+            this.cmBoxSeller.Name = "cmBoxSeller";
+            this.cmBoxSeller.Size = new System.Drawing.Size(151, 32);
+            this.cmBoxSeller.TabIndex = 7;
+            // 
+            // lblSeller
+            // 
+            this.lblSeller.AutoSize = true;
+            this.lblSeller.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSeller.Location = new System.Drawing.Point(211, 83);
+            this.lblSeller.Name = "lblSeller";
+            this.lblSeller.Size = new System.Drawing.Size(65, 24);
+            this.lblSeller.TabIndex = 8;
+            this.lblSeller.Text = "Myyjä";
+            // 
             // Lisaaminen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.pnlControls);
             this.Controls.Add(this.panel1);
             this.Name = "Lisaaminen";
@@ -262,6 +390,9 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.pnlControls.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numAmount)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -287,5 +418,16 @@
         private System.Windows.Forms.Button btnEmpty;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblAmmount;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblService;
+        private System.Windows.Forms.ComboBox cmBoxTime;
+        private System.Windows.Forms.ComboBox cmBoxService;
+        private System.Windows.Forms.NumericUpDown numAmount;
+        private System.Windows.Forms.DateTimePicker dtmSellTime;
+        private System.Windows.Forms.Label lblSeller;
+        private System.Windows.Forms.ComboBox cmBoxSeller;
     }
 }
