13d90f9e03ca85e08c8a1a0da253d465806bce16
Konttila Arttu <ao187046@gradia.fi>
2019-08-08 12:22:44 +0300

    Sql yhteys luotu

--- Lahjakorttiappi/DatabaseController/DatabaseController.cs
+++ Lahjakorttiappi/DatabaseController/DatabaseController.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Lahjakorttiappi.DatabaseController
 {
     class DatabaseController
     {
+        //luo sql yhteyden
+        SqlConnection connect = new SqlConnection();
+        public DatabaseController()
+        {
+            connect.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=\Lahjakorttiappi\Lahjakortti.mdf;Integrated Security=True";
+        }
+        
     }
 }
